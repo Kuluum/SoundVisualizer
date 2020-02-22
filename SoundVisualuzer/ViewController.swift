c6501460fd5fb524dd883c85cf58f272209bcfef
DNosov <dnosov@spbtv.com>
2020-02-22 15:59:57 +0300

    Initial Commit

---
+++ SoundVisualuzer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SoundVisualuzer
+//
+//  Created by Daniil on 22.02.2020.
+//  Copyright © 2020 Kuluum. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
